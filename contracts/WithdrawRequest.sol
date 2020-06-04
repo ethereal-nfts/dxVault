4fefd6081a0ff7da68be984620e45c1311449749
Ethereal <0_ethereal_0@protonmail.com>
2020-06-03 21:37:24 -0700

    Add WithdrawRequest imports and initializer.

--- contracts/WithdrawRequest.sol
+++ contracts/WithdrawRequest.sol
@@ -1,6 +1,19 @@
 pragma solidity 0.5.17;
 
+import "@gnosis.pm/safe-contracts/contracts/base/Module.sol";
+import "@gnosis.pm/safe-contracts/contracts/base/ModuleManager.sol";
+import "@openzeppelin/contracts-ethereum-package/contracts/math/SafeMath.sol";
+import "@openzeppelin/upgrades/contracts/Initializable.sol";
 
-contract WithdrawRequest {
+import "./libraries/Reputation.sol";
 
+
+contract WithdrawRequest is Module, Initializable {
+    Reputation private dxDaoRep;
+    uint public snapshotBlock;
+
+    function initialize (Reputation _dxDaoRep, uint _snapshotBlock) public initializer {
+        dxDaoRep = _dxDaoRep;
+        snapshotBlock = _snapshotBlock;
+    }
 }
